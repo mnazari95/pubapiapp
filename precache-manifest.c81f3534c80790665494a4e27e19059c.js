0db902680605275592941a2ba497666ae2681934
HafizN <hafizn2013@outlook.com>
2020-02-05 13:37:33 -0800

    Updates

---
+++ precache-manifest.c81f3534c80790665494a4e27e19059c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe3d4a14251b2d02da02ff3d1a5754ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d63c26d2baa1129c1e78",
+    "url": "/static/css/main.e042e4ba.chunk.css"
+  },
+  {
+    "revision": "1f6591e5c6186b13215d",
+    "url": "/static/js/2.660f9449.chunk.js"
+  },
+  {
+    "revision": "ce028d5e2b0507b0fb507336501c1edf",
+    "url": "/static/js/2.660f9449.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d63c26d2baa1129c1e78",
+    "url": "/static/js/main.c36216be.chunk.js"
+  },
+  {
+    "revision": "11fb3c1e3c00bec3573d",
+    "url": "/static/js/runtime-main.6f2c1448.js"
+  }
+]);
